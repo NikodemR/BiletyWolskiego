0e9342bf9b511a2aa424756ca267a917c91cc280
Jakub G. <jak.gor.92@gmail.com>
2016-07-05 20:17:39 +0200

    Wolski to jest spoko gość

---
+++ Bilety/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Bilety
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.ValueMember = "Value";
+            comboBox1.DisplayMember = "Text";
+            List<object> lista = new List<object>();
+            foreach (var value in Enum.GetValues(typeof(KartaMiejska.RodzajUczelni)))
+            {
+                lista.Add(new { Value = (int)value, Text = value.ToString() });
+            }
+            comboBox1.DataSource = lista;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KartaMiejska karta = new KartaMiejska();
+            karta.NrAlbumu = textBox1.Text;
+            karta.TypUczelni = comboBox1.SelectedValue.ToString();
+            textBox2.Text = PobierzDane.ZrobLink(karta);
+        }
+    }
+}
---
+++ Bilety/PobierzDane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilety
+{
+    class PobierzDane
+    {
+
+        public static string ZrobLink(KartaMiejska karta)
+        {
+            string link = "http://www.kkm.krakow.pl/pl/sprawdz-waznosc-biletow-zapisanych-na-karcie/index,1.html?cityCardType=" + karta.TypUczelni + "&dateValidity =" + DateTime.Today.ToString("dd-MM-yyyy") + "&identityNumber=" + karta.NrAlbumu + "&sprawdz_kkm=Sprawdź";
+            var html = new System.Net.WebClient().DownloadString(link);
+            string dataWaznosci = html.Substring(html.IndexOf("Data ko") +27, 10);
+            return dataWaznosci;
+
+        }
+        
+        /*public static string SprawdzDateWaznosciKarty()
+        {
+            
+            var html = new System.Net.WebClient().DownloadString();
+            string dataWaznosci = html.Substring(html.IndexOf("2016-07-23"), 10);
+            return dataWaznosci;
+        }*/
+    }
+}
