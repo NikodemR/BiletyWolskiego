c10922781cb41d42f1c151fec902fcd7ff18630c
Jakub G. <jak.gor.92@gmail.com>
2016-07-06 20:19:18 +0200

    Jestem dumny z Wolskiego!

--- Bilety/PobierzDane.cs
+++ Bilety/PobierzDane.cs
@@ -11,11 +11,9 @@ namespace Bilety
 
         public static string SprawdzDateWaznosci(KartaMiejska karta)
         {
-            string link = StworzLink(karta);
-            var html = new System.Net.WebClient().DownloadString(link);
-            string dataWaznosci = html.Substring(html.IndexOf("Data ko") +27, 10);
-            return dataWaznosci;
-
+            string adresUrl = StworzLink(karta);
+            string html = WczytajKodStrony(adresUrl);
+            return PobierzDateZeStrony(html);
         }
 
         private static string StworzLink(KartaMiejska karta)
@@ -23,7 +21,16 @@ namespace Bilety
             return "http://www.kkm.krakow.pl/pl/sprawdz-waznosc-biletow-zapisanych-na-karcie/index,1.html?cityCardType=" +
                 karta.TypUczelni + "&dateValidity =" + DateTime.Today.ToString("dd-MM-yyyy") + "&identityNumber=" +
                 karta.NrAlbumu + "&sprawdz_kkm=Sprawdź";
+            //Wolski królem Polski
         }
-       
+        private static string WczytajKodStrony(string link)
+        {
+            return new System.Net.WebClient().DownloadString(link);
+        }
+        private static string PobierzDateZeStrony(string html)
+        {
+            return html.Substring(html.IndexOf("Data ko") + 27, 10);
+        }
+
     }
 }
