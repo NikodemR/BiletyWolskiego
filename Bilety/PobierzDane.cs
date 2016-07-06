f12ade8b997e33974e457ca42e502d9e7938ee67
Jakub G. <jak.gor.92@gmail.com>
2016-07-07 00:17:43 +0200

    GDS super jest!

--- Bilety/PobierzDane.cs
+++ Bilety/PobierzDane.cs
@@ -7,13 +7,13 @@ namespace Bilety
     {
         public static Func<KartaMiejska, string> SprawdzDateWaznosciKarty = k => PobierzDaneZeStrony(WczytajKodHtmlStrony(UtworzLink(k)));
 
-        private static readonly Func<KartaMiejska, string> UtworzLink = s => "http://www.kkm.krakow.pl/pl/sprawdz-waznosc-biletow-zapisanych-na-karcie/index,1.html?cityCardType=" +
-                s.TypUczelni + "&dateValidity =" + DateTime.Today.ToString("dd-MM-yyyy") + "&identityNumber=" +
-                s.NrAlbumu + "&sprawdz_kkm=Sprawdź";
+        private static readonly Func<KartaMiejska, string> UtworzLink = k => "http://www.kkm.krakow.pl/pl/sprawdz-waznosc-biletow-zapisanych-na-karcie/index,1.html?cityCardType=" +
+                k.TypUczelni + "&dateValidity =" + DateTime.Today.ToString("dd-MM-yyyy") + "&identityNumber=" +
+                k.NrAlbumu + "&sprawdz_kkm=Sprawdź";
 
         private static readonly Func<string, string> WczytajKodHtmlStrony = s => new WebClient().DownloadString(s);
 
         private static readonly Func<string, string> PobierzDaneZeStrony = s => s.Substring(s.IndexOf("Data ko") + 27, 10);
-        //
+
     }
 }
