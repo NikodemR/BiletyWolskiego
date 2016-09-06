a234420d271a0430642e03fa1f199d4cac4bbdd1
Szymon <Szymon@SZYMON-KOMPUTER>
2016-09-07 00:07:27 +0200

    jhjvk

--- Bilety/Wiadomosc.cs
+++ Bilety/Wiadomosc.cs
@@ -1,14 +1,20 @@
-﻿using System.Net.Mail;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Mail;
 
 namespace Bilety
 {
-    public static class Wiadomosc
+    static class Wiadomosc
     {
         public static string AdresMail { get; set; }
 
-        public static string TrescWiadomosci(string dataWaznosci, int ileDniZostalo)
+        public static string TrescWiadomosci(string DataWaznosci, int IleDniZostalo)
         {
-            return "Data ważności karty miejskiej kończy się: " + dataWaznosci + ". Do zakończenia ważności karty miejskiej pozostalo " + ileDniZostalo.ToString() + " dni.";
+            return "Data ważności karty miejskiej kończy się: " + DataWaznosci + ". Do zakończenia ważności karty miejskiej pozostalo " + IleDniZostalo.ToString() + " dni.";
         }
 
         public static void WyslijWiadomosc(KartaMiejska karta)
@@ -16,17 +22,13 @@ namespace Bilety
             karta.DataWaznosci = PobierzDane.SprawdzDateWaznosci(karta);
             karta.IleDniZostalo = PobierzDane.ObliczIleDniZostalo(karta);
             MailMessage mail = new MailMessage("waznosc.kkm@gmail.com", karta.AdresMail, "Przypomnienie o końcu ważności Karty Komunikacji Miejskiej.",
-                 TrescWiadomosci(karta.DataWaznosci, karta.IleDniZostalo));
-            
-            SmtpClient client = new SmtpClient();
-            client.Host = "smtp.gmail.com";
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                 Wiadomosc.TrescWiadomosci(karta.DataWaznosci, karta.IleDniZostalo));
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
             client.Port = 587;
-            client.UseDefaultCredentials = false;
             client.Credentials = new System.Net.NetworkCredential("waznosc.kkm@gmail.com", "kanarylubiadolary");
             client.EnableSsl = true;
             client.Send(mail);
-
         }
     }
 }
